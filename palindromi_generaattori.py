1833cbab1ff79cf520c3877771a23489bdcdf531
Mathias von Essen <mathias.vonessen@no-reply.com>
2021-09-12 10:40:11 +0100

    eka versio, joka tulostaa palindromimerkkijonoja

--- palindromi_generaattori.py
+++ palindromi_generaattori.py
@@ -2,14 +2,14 @@
 Tästä tehdään ohjelma joka luo palindromeja
 """
 
+
 import itertools
 from palindromi.tunnistin import onko_palindromi
 from palindromi.merkkijono import luo_merkkijonot, AAKKOSET
 
 palindromit = []
 
-
-for r in range(4, 10):
+for r in range(4, 6):https://www.amazon.co.uk/Wacom-Cintiq-Creative-Display-DTK-2260/dp/B07TR7YQ8Y
     print(f'lasketaan kaikki {r} merkkiä pitkät merkkijonot', end=" ")
     for s in itertools.product(AAKKOSET, repeat=r):
         mjono = ''.join(s)
@@ -20,8 +20,7 @@ for r in range(4, 10):
     print('')
 
 
-for p in palindromit:
-    print(p)
+print(f"löytyi {len(palindromit)} palindromia")
 
 
 
