8a42a019554a167d2dc6f2113f14cb0023da734c
Mathias <mathias.vonessen@gmail.com>
2021-10-24 19:31:21 +0100

    Sprite joka on sprite

--- peli_hommia/grafiikka/sprite_1.py
+++ peli_hommia/grafiikka/sprite_1.py
@@ -5,20 +5,28 @@ from pygame.constants import K_UP, K_DOWN, K_LEFT, K_RIGHT, QUIT, K_ESCAPE, K_a
 class Hahmo(pygame.sprite.Sprite):
     def __init__(self, leveys, korkeus, paikka_x, paikka_y):
         super().__init__()
-        self.image = pygame.Surface([leveys, korkeus])
-        self.image.fill((255, 0, 0))
+        #self.image = pygame.Surface([leveys, korkeus])
+        #self.image.fill((255, 0, 0))
+        self.image = pygame.image.load('sprite.png')
         self.rect = self.image.get_rect()
+        # rect = rectangle eli suorakaide (neliö)
         self.rect.center = [paikka_x, paikka_y]
 
+    # args = lista
+    # kwargs = sanakirja (kw=keyword => avain)
     def update(self, *args, **kwargs) -> None:
-        x, y = self.rect.center
-        x_siirto, y_siirto = 0, 0
+        # käyttää vain kwargs ja 'x' ja 'y'
+
+        alkup_x, alkup_y = self.rect.center
+        x_siirto = 0
+        y_siirto = 0
+
         if 'y' in kwargs:
             y_siirto = kwargs['y']
         if 'x' in kwargs:
             x_siirto = kwargs['x']
 
-        self.rect.center = (x + x_siirto, y + y_siirto)
+        self.rect.center = (alkup_x + x_siirto, alkup_y + y_siirto)
 
 
 clock = pygame.time.Clock()
@@ -29,11 +37,12 @@ tausta = pygame.Surface([1920, 1080])
 tausta.fill((0,0,0))
 
 ruutu = pygame.display.set_mode(ruudun_koko)
-
 m = Hahmo(50, 50, 100, 100)
+o = Hahmo(90, 50, 700, 200)
 
-minun_ryhma = pygame.sprite.Group()
-minun_ryhma.add(m)
+hahmo_ryhma = pygame.sprite.Group()
+hahmo_ryhma.add(m)
+hahmo_ryhma.add(o)
 
 while True:
     for event in pygame.event.get():
@@ -41,18 +50,25 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
         if event.type == pygame.KEYDOWN:
-            x, y = 0, 0
+            x = 0
+            y = 0
             if event.key == K_UP:
                 # painettu nuolta ylös
-                y = 10
+                y = -10
 
             elif event.key == K_DOWN:
                 # painettu nuolta alas
-                y = -10
+                y = 10
 
-        pygame.display.flip()
+            if event.key == K_LEFT:
+                x = -10
+            elif event.key == K_RIGHT:
+                x = 10
+
         ruutu.blit(tausta, (0,0))
-        minun_ryhma.draw(ruutu)
-        minun_ryhma.update(x=x, y=y)
+        hahmo_ryhma.update(x=x, y=y)
+        hahmo_ryhma.draw(ruutu)
+        pygame.display.flip()
         clock.tick(60)
